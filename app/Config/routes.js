3ba519b94363a88100ce262c61b6b921af8506a4
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-03-02 15:16:43 -0800

    Install react-native-elements for icons for TabNavigator

--- app/Config/routes.js
+++ app/Config/routes.js
@@ -1,7 +1,6 @@
-import {StackNavigator} from 'react-navigation';
+import {StackNavigator, TabNavigator} from 'react-navigation';
 import Home from '../screens/Home';
 import Login from '../screens/Login';
-
 
 export default StackNavigator({
   Home: {
