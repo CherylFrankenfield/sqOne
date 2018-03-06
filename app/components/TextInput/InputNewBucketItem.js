282caac00adec9635f3f3fd4c2e2aef3131f252d
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-03-06 13:23:33 -0800

    Delete phantom src folder

--- app/components/TextInput/InputNewBucketItem.js
+++ app/components/TextInput/InputNewBucketItem.js
@@ -1,33 +1,54 @@
-import React, {PropTypes} from 'react';
-import {View, Text, TouchableHighlight, TextInput} from 'react-native';
-import styles from './styles'
+import React from 'react';
+import {View, Text, TouchableHighlight, TextInput, StyleSheet, TouchableOpacity} from 'react-native';
+// import styles from './styles'
 import {Container, InputGroup, Input, Button} from 'native-base';
 
 class InputNewBucketItem extends React.Component {
+  handleSubmit(text, field) {
+    console.log(text);
+  }
+
   render() {
     return(
-      <View>
-        <InputGroup borderType='underline'>
-          <Input placeholder='New Bucket List Item:'/>
-        </InputGroup>
-        <InputGroup borderType='underline'>
-          <Input placeholder='Date:'/>
-        </InputGroup>
-        <InputGroup borderType='underline'>
-          <Input placeholder='Price:'/>
-        </InputGroup>
-        <InputGroup borderType='underline'>
-          <Input placeholder='Location:'/>
-        </InputGroup>
-        <InputGroup borderType='underline'>
-          <Input placeholder='Notes:'/>
-        </InputGroup>
-        <TouchableHighlight>
+      <View style={styles.container}>
+        <TextInput
+          placeholder='test'
+          onSubmit={(text) => this.handleSubmit(text,i)}
+        />
+        <TouchableOpacity>
           <Button block><Text> ADD EVENT </Text></Button>
-        </TouchableHighlight>
+        </TouchableOpacity>
       </View>
     );
   }
 }
 
 export default InputNewBucketItem;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'honeydew',
+    justifyContent: 'center',
+  },
+})
+// <View>
+//   <InputGroup borderType='underline'>
+//     <Input placeholder='New Bucket List Item:'/>
+//   </InputGroup>
+//   <InputGroup borderType='underline'>
+//     <Input placeholder='Date:'/>
+//   </InputGroup>
+//   <InputGroup borderType='underline'>
+//     <Input placeholder='Price:'/>
+//   </InputGroup>
+//   <InputGroup borderType='underline'>
+//     <Input placeholder='Location:'/>
+//   </InputGroup>
+//   <InputGroup borderType='underline'>
+//     <Input placeholder='Notes:'/>
+//   </InputGroup>
+//   <TouchableHighlight>
+//     <Button block><Text> ADD EVENT </Text></Button>
+//   </TouchableHighlight>
+// </View>
--- app/screens/NewEvent.js
+++ app/screens/NewEvent.js
@@ -3,7 +3,13 @@ import {View,Text, TouchableHighlight} from 'react-native';
 import {Container, InputGroup, Input, Button} from 'native-base';
 import {InputNewBucketItem} from '../components/TextInput';
 
+
 class NewEvent extends React.Component {
+
+  handleNewEventSubmission() {
+    console.log('new event works')
+  };
+
   render(){
     return(
       <Container>
@@ -16,27 +22,3 @@ class NewEvent extends React.Component {
 }
 
 export default NewEvent;
-
-
-// <Container>
-//   <Content>
-//     <InputGroup borderType='underline'>
-//       <Input placeholder='New Bucket List Item:'/>
-//     </InputGroup>
-//     <InputGroup borderType='underline'>
-//       <Input placeholder='Date:'/>
-//     </InputGroup>
-//     <InputGroup borderType='underline'>
-//       <Input placeholder='Price:'/>
-//     </InputGroup>
-//     <InputGroup borderType='underline'>
-//       <Input placeholder='Location:'/>
-//     </InputGroup>
-//     <InputGroup borderType='underline'>
-//       <Input placeholder='Notes:'/>
-//     </InputGroup>
-//     <TouchableHighlight>
-//       <Button block><Text> ADD EVENT </Text></Button>
-//     </TouchableHighlight>
-//   </Content>
-// </Container>
