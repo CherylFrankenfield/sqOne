cea06323af6529e95081bf13af0120737de77c98
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-03-22 14:29:50 -0700

    Add local UI state eventName to NewEvent component

--- src/screens/NewEvent.js
+++ src/screens/NewEvent.js
@@ -4,30 +4,74 @@ import {Container, InputGroup, Input, Button} from 'native-base';
 // import InputNewBucketItem from '../components/TextInput';
 
 class NewEvent extends React.Component {
+  state = {
+    eventName: ''
+  };
+
+  eventNameChangedHandler = val => {
+    this.setState({
+      eventName: val
+    });
+  };
+
+  eventSubmitHandler = () => {
+    if (this.state.placeName.trim() === '') {
+      return;
+    }
+    this.setState(prevState => {
+      return {
+        bucketlist: prevState.bucketlist.concat(prevState.eventName)
+      };
+    });
+  };
+
   render() {
     return (
-      <View>
-        <InputGroup borderType='underline'>
-          <Input placeholder='New Bucket List Item:'/>
-        </InputGroup>
-        <InputGroup borderType='underline'>
-          <Input placeholder='Date:'/>
-        </InputGroup>
-        <InputGroup borderType='underline'>
-          <Input placeholder='Price:'/>
-        </InputGroup>
-        <InputGroup borderType='underline'>
-          <Input placeholder='Location:'/>
-        </InputGroup>
-        <InputGroup borderType='underline'>
-          <Input placeholder='Notes:'/>
+      <View style={styles.inputContainer}>
+        <InputGroup
+          borderType='underline'
+          style={styles.eventInput}>
+          <Input
+            placeholder='New Bucket List Item:'
+            value={this.state.eventName}
+            onChangeText={this.eventNameChangedHandler}
+          />
         </InputGroup>
         <TouchableHighlight>
-          <Button block><Text> ADD EVENT </Text></Button>
+          <Button
+            style={styles.eventButton}
+            onPress={this.eventSubmitHandler}>
+            <Text> ADD EVENT </Text>
+          </Button>
         </TouchableHighlight>
       </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 40,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%'
+  },
+  eventButton: {
+    marginTop: 10,
+    width: '30%',
+  },
+  eventInput: {
+    marginTop: 10,
+    width: '85%'
+  }
+});
+
 export default NewEvent;
+
+
+
+
+// <Input placeholder='Date:'/>
+// <Input placeholder='Price:'/>
+// <Input placeholder='Location:'/>
+// <Input placeholder='Notes:'/>
